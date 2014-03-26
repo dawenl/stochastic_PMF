f11b2e0e8d32dcb6d8db2e8e657dd859a79b5c61
Dawen Liang <dl2771@columbia.edu>
2014-03-26 02:38:27 -0400

    reformat

--- code/msd_rec/processLastfmTags.py
+++ code/msd_rec/processLastfmTags.py
@@ -63,42 +63,44 @@ def sanitize(tag):
 filtered_tags = (# favorate/like/love/blabla
                  'favorites', 'Favorite', 'Favourites', 'favourite', 'favorite songs', 'Favourite Songs', 'favorite song', 
                  'songs i love', 'lovedbybeyondwithin', 'Love it', 'love at first listen', 'fav', 'my favorite', 'top 40', 
-                 'songs I absolutely love', 'favs', 'My Favorites', 'Favorite Artists', 'All time favourites', 'personal favourites',
-                 'favouritestreamable', 'favorite tracks', 'Favorite Bands', 'like it', 'I love this song', 'rex ferric faves',
-                 'love to death', 'my gang 09', 'My Favourites', 'BeatbabeBop selection', 'I Like It', 'newbest', 'top',
-                 'IIIIIIIIII AMAZING TRACK :D IIIIIIIIII', 'best songs of the 80s', 'LOVE LOVE LOVE', 'i love it', 'most loved',
-                 'favorite by this group', 'amayzes loved', 'DJPMan-loved-tracks', 'best of 2008', 'loved', 'Makes Me Smile',
-                 '77davez-all-tracks', 'My pop music', 'best songs ever', 'favorite by this singer', 'I like', 'my music',
-                 'Soundtrack Of My Life', 'UK top 40', 'Like', 'malloy2000 playlist - top songs - classical to metal', 'loved tracks',
+                 'songs I absolutely love', 'favs', 'My Favorites', 'Favorite Artists', 'All time favourites', 
+                 'personal favourites', 'favouritestreamable', 'favorite tracks', 'Favorite Bands', 'like it', 
+                 'I love this song', 'rex ferric faves', 'love to death', 'my gang 09', 'My Favourites', 
+                 'BeatbabeBop selection', 'I Like It', 'newbest', 'top', 'IIIIIIIIII AMAZING TRACK :D IIIIIIIIII', 
+                 'best songs of the 80s', 'LOVE LOVE LOVE', 'i love it', 'most loved',
+                 'favorite by this group', 'amayzes loved', 'DJPMan-loved-tracks', 'best of 2008', 'loved', 
+                 'Makes Me Smile', '77davez-all-tracks', 'My pop music', 'best songs ever', 'favorite by this singer', 
+                 'I like', 'my music', 'Soundtrack Of My Life', 'UK top 40', 'Like', 
+                 'malloy2000 playlist - top songs - classical to metal', 'loved tracks',
                  'top artists', 'all time favorites', 'best songs of the 00s', 'favourite tracks', 'Solomusika-Loved', 
                  'all time faves', 'british i like', 'Jills Station', 'de todo mio favoritos', 'Faves', 'Fave', 
                  'acclaimed music top 3000'
 
                  # great/awesome/blabla
-                 'kick ass', 'wonderful', 'excellent', 'Great Lyricists', 'badass', 'awesomeness', 'great song', 'Awesome', 'cool',  
-                 'amazing', 'good', 'nice', 'sweet', 'best', 'FUCKING AWESOME', 'lovely', 'Good Stuff', 'brilliant', 'feel good', 
-                 'perfect', 'all the best', 'cute', 'the best', '<3', 'interesting', 'feelgood', 'pretty', 'i feel good', 'good shit',
-                 'good music', 'good song', 'great songs', 'yeah', 'best song ever', 'wow', 'worship', 'makes me happy', 'ok',
-                 'damned good', 'underrated', 'Perfection',
+                 'kick ass', 'wonderful', 'excellent', 'Great Lyricists', 'badass', 'awesomeness', 'great song', 'Awesome',
+                 'cool', 'amazing', 'good', 'nice', 'sweet', 'best', 'FUCKING AWESOME', 'lovely', 'Good Stuff', 'brilliant',
+                 'feel good', 'perfect', 'all the best', 'cute', 'the best', '<3', 'interesting', 'feelgood', 'pretty', 
+                 'i feel good', 'good shit', 'good music', 'good song', 'great songs', 'yeah', 'best song ever', 'wow', 
+                 'worship', 'makes me happy', 'ok', 'damned good', 'underrated', 'Perfection',
                  
                  # rating
                  '1', '3', '4', '5', '4 Stars', '3 stars', '4 Star', '3 star', '3-star',
                  
                  # year
-                 '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', 
-                 '2006', '2007', '2008', '2009', '2010', '00s', '10s', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', 
-                 '20th Century', '21st century', "50's", '50s', "60's", '60s', '60s Gold', "70's", '70s', "80's", '80s', '80s Pop',
-                 '80s rock', "90's", '90s', '90s Rock',
+                 '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', 
+                 '2005', '2006', '2007', '2008', '2009', '2010', '00s', '10s', '1950s', '1960s', '1970s', '1980s', '1990s',
+                 '2000s', '20th Century', '21st century', "50's", '50s', "60's", '60s', '60s Gold', "70's", '70s', "80's", 
+                 '80s', '80s Pop', '80s rock', "90's", '90s', '90s Rock',
                  
                  # descriptive
-                 'songwriter', 'singer-songwriter', 'cover', 'covers', 'seen live', 'heard on Pandora', 'title is a full sentence',
-                 'Retro', 'Miscellaneous', 'collection', 'billboard number ones', 'ost', 'cover song', 'singer songwriter', 'new',
-                 'download', 'over 5 minutes long', 'Soundtracks', 'under two minutes', 'albums I own', 'cover songs', 'Radio',
-                 'heard on last-fm',
+                 'songwriter', 'singer-songwriter', 'cover', 'covers', 'seen live', 'heard on Pandora', 
+                 'title is a full sentence', 'Retro', 'Miscellaneous', 'collection', 'billboard number ones', 'ost', 
+                 'cover song', 'singer songwriter', 'new', 'download', 'over 5 minutes long', 'Soundtracks', 
+                 'under two minutes', 'albums I own', 'cover songs', 'Radio', 'heard on last-fm',
       
                  # I don't know what you are talking about
-                 'buy', 'lol', 'us', 'other', '2giveme5', 'i am a party girl here is my soundtrack', 'names', 'Tag', 'check out',
-                 'f', 'test', 'out of our heads', 'me', 'I want back to the 80s', '9 lbs hammer', 'yes',
+                 'buy', 'lol', 'us', 'other', '2giveme5', 'i am a party girl here is my soundtrack', 'names', 'Tag', 
+                 'check out', 'f', 'test', 'out of our heads', 'me', 'I want back to the 80s', '9 lbs hammer', 'yes',
                  )  
 
 # <codecell>
