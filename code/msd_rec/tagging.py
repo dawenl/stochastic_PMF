55ebc0e50bd98a3ad400281d031f13c3db032e42
Dawen Liang <dl2771@columbia.edu>
2014-04-28 17:55:58 -0400

    sync

--- code/msd_rec/tagging.py
+++ code/msd_rec/tagging.py
@@ -48,7 +48,7 @@ def per_tag_prec_recall(tags_predicted_binary, tags_true_binary):
 
 # <codecell>
 
-# take tracks with at least 20 tags from test set
+# take tracks with at least 20 tags 
 y_test = None
 
 test_tracks_selected = list()
@@ -89,8 +89,12 @@ for (i, tid) in enumerate(train_tracks_subset):
     tdir = os.path.join('vq_hist', '/'.join(tid[2:5]))
     vq = np.load(os.path.join(tdir, '%s_K%d.npy' % (tid, K))).ravel()
     bot = np.load(os.path.join(tdir, '%s_BoT.npy' % tid))
-    bot *= (vq.max() / 100.0)
+    bot[bot > 0] = 2
     X[i] = np.hstack((vq, bot))
+
+# <codecell>
+
+bar(np.arange(D), X[1000])
 
 # <codecell>
 
@@ -104,7 +108,7 @@ for (i, tid) in enumerate(test_tracks_selected):
 # <codecell>
 
 reload(pnmf)
-n_components = 50
+n_components = 100
 coder = pnmf.PoissonNMF(n_components=n_components, random_state=98765, verbose=True)
 
 # <codecell>
@@ -113,13 +117,19 @@ coder.fit(X)
 
 # <codecell>
 
-# plot 10 "topics"
-indices = np.random.choice(n_components, size=10, replace=False)
-figure(figsize=(16, 15))
-for i in xrange(10):
-    subplot(5, 2, i+1)
-    bar(np.arange(D), coder.Eb[indices[i]])
+# randomly plot 30 "topics"
+indices = np.random.choice(n_components, size=30, replace=False)
+figure(figsize=(45, 15))
+for i in xrange(30):
+    subplot(10, 3, i+1)
+    topic = coder.Eb[indices[i]]
+    # properly normalize the BoT dimensions for visualization purposes
+    topic[K:] /= topic[K:].max()
+    topic[K:] *= topic[:K].max()
+    bar(np.arange(D), topic)
+    axvline(x=K, color='red')
     title('Component #%d' % indices[i])
+#savefig('dict.eps')
 
 # <codecell>
 
@@ -136,29 +146,37 @@ Et = tagger.transform(X_test)
 # <codecell>
 
 tags_predicted = Et.dot(coder.Eb[:, K:])
+n_samples, n_tags = tags_predicted.shape
 
-div_factor = 2.5
+print tags_predicted.min(), tags_predicted.max()
+
+div_factor = 4.75
 tags_predicted = tags_predicted - div_factor * np.mean(tags_predicted, axis=0)
 
 # <codecell>
 
-predictat = 50
+predictat = 20
 tags_predicted_binary = construct_pred_mask(tags_predicted, predictat)
 tags_true_binary = (y_test > 0)
-
-# <codecell>
-
-n_samples, n_tags = tags_predicted.shape
-tags_random_binary = np.zeros((n_samples, n_tags), dtype=bool)
-for i in xrange(n_samples):
-    idx = np.random.choice(n_tags, size=predictat, replace=False)
-    tags_random_binary[i, idx] = 1
 
 # <codecell>
 
 prec, recall = per_tag_prec_recall(tags_predicted_binary, tags_true_binary)
 print np.mean(prec), np.std(prec) / sqrt(n_tags)
 print np.mean(recall), np.std(recall) / sqrt(n_tags)
+
+# <codecell>
+
+prec, recall = per_tag_prec_recall(tags_predicted_binary, tags_true_binary)
+print np.mean(prec), np.std(prec) / sqrt(n_tags)
+print np.mean(recall), np.std(recall) / sqrt(n_tags)
+
+# <codecell>
+
+tags_random_binary = np.zeros((n_samples, n_tags), dtype=bool)
+for i in xrange(n_samples):
+    idx = np.random.choice(n_tags, size=predictat, replace=False)
+    tags_random_binary[i, idx] = 1
 
 # <codecell>
 
@@ -172,7 +190,20 @@ print np.mean(recall), np.std(recall) / sqrt(n_tags)
 
 # <codecell>
 
-online_coder = pnmf.OnlinePoissonNMF()
+reload(pnmf)
+
+n_components = 100
+online_coder = pnmf.OnlinePoissonNMF(n_components=n_components, batch_size=10, n_pass=1, random_state=98765, verbose=True)
+
+# <codecell>
+
+online_coder.fit(X)
+
+# <codecell>
+
+
+# <codecell>
+
 
 # <headingcell level=1>
 
